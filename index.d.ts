4f2030d6d3504fbb7f0758b208123ae87cf5a224
Nathan Simpson <hello@nathansimpson.design>
2021-10-10 17:58:50 +1100

    Remove incorrect default exports in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -4,19 +4,13 @@ type hexColor = string;
 type wcagScore = "AAA" | "AA" | "AA+" | "FAIL";
 
 /** Derives an accessible text color for a background. */
-export default function hexAlly(
+export function hexAlly(
   input: hexColor,
   config?: { dark: hexColor; light: hexColor }
 ): hexColor;
 
 /** Returns the contrast ratio between two colors. */
-export default function getContrastRatio(
-  inputA: hexColor,
-  inputB: hexColor
-): number;
+export function getContrastRatio(inputA: hexColor, inputB: hexColor): number;
 
 /** Returns the WCAG score determined from two colors. */
-export default function getWcagScore(
-  inputA: hexColor,
-  inputB: hexColor
-): wcagScore;
+export function getWcagScore(inputA: hexColor, inputB: hexColor): wcagScore;
